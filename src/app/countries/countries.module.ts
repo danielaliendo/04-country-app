5fdc814e4d901fec2c61a1b8a3485fcf3dd271e2
Daniela Liendo <daniela@ualabee.com>
2023-05-08 00:09:48 +0200

    countries routing module and countries module were created

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent
+  ],
   imports: [
     CommonModule
   ]
