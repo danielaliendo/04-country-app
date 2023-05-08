dc9bf8a2b7734a5e9a7a9f5969c86ac127f6ecc5
Daniela Liendo <daniela@ualabee.com>
2023-05-08 21:32:37 +0200

    search box component was declared and exported from shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,15 +5,15 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
-
-
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     HomePageComponent,
     SidebarComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule,
@@ -23,7 +23,9 @@ import { ContactPageComponent } from './pages/contact-page/contact-page.componen
     AboutPageComponent,
     HomePageComponent,
     SidebarComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    SearchBoxComponent
   ]
 })
+
 export class SharedModule { }
