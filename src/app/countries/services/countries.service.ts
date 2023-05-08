f026fe29adcef12864193dee26f04950b7f6b0bc
Daniela Liendo <daniela@ualabee.com>
2023-05-08 21:50:34 +0200

    searchCapital method was created

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Country } from '../interfaces/country';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 
 export class ServiceNameService {
 
-  constructor(private httpClient: HttpClient) { }
+  private API_URL: string = 'https://restcountries.com/v3.1'
+
+  constructor(private http: HttpClient) { }
+
+  searchCapital(term: string): Observable<Country[]> {
+    return this.http.get<Country[]>(`${this.API_URL}/capital/${term}`)
+  }
 
 }
