794992aea3238431fd85d2828010bb9072eb7eaf
Daniela Liendo <daniela@ualabee.com>
2023-05-08 21:31:10 +0200

    search box component was created

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-search-box',
+  selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styles: [
   ]
 })
-export class SearchBoxComponent {
+
+export class SearchBoxComponent implements OnInit {
+
+  @Input()
+  public type: string = 'text'
+
+  @Input()
+  public placeholder: string = ''
+
+  @Output()
+  public onValue = new EventEmitter<string>()
+
+  constructor() {
+
+  }
+
+  emitValue(value: string): void {
+    this.onValue.emit(value)
+  }
+
+  ngOnInit(): void {
+
+    if (!this.type) {
+      throw new Error('type string is required.');
+    } else if (!this.placeholder) {
+      throw new Error('placeholder string is required.');
+    }
+
+  }
 
 }
