573021f956e032958ddadddc8704f7ed15af8456
Daniela Liendo <daniela@ualabee.com>
2023-05-09 09:16:04 +0200

    searchByCountry and searchByRegion methods were created

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -11,8 +11,31 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+  public url: string = ''
+
   searchByCapital(term: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.API_URL}/capital/${term}`)
+    this.url = `${this.API_URL}/capital/${term}`
+    return this.getResults(this.url)
+  }
+
+  searchByCountry(term: string): Observable<Country[]> {
+    this.url = `${this.API_URL}/name/${term}`
+    return this.getResults(this.url)
+  }
+
+  searchByRegion(term: string): Observable<Country[]> {
+    this.url = `${this.API_URL}/region/${term}`
+    return this.getResults(this.url)
+  }
+
+  getResults(url: string) {
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError((error) => {
+        console.error(error)
+        return of([])
+      })
+    )
   }
 
 }
