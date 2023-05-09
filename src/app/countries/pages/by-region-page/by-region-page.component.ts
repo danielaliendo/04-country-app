85c394f5d11bc68b106133372a35421901baf5d4
Daniela Liendo <daniela@ualabee.com>
2023-05-09 21:43:50 +0200

    regions were added such as buttons to select and search countries by selected region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania'
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,16 +12,23 @@ import { CountriesService } from '../../services/countries.service';
 })
 
 export class ByRegionPageComponent {
+
   public countries: Country[] = []
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  public isLoading:boolean = false
+  public selectedRegion?: Region
 
   constructor(private contriesService: CountriesService) {
 
   }
 
-  searchByRegion(term: string) {
+  searchByRegion(term: Region) {
+    this.selectedRegion = term
+    this.isLoading = true
     this.contriesService.searchByRegion(term)
     .subscribe((countries: Country[]) => {
       this.countries = countries
+      this.isLoading = false
     })
   }
 }
