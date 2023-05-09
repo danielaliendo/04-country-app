148f66d08fee7ec5edb964840750b0c454797b72
Daniela Liendo <daniela@ualabee.com>
2023-05-09 19:53:06 +0200

    country page component functionality was added

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Country } from './../../interfaces/country';
+import { CountriesService } from './../../services/countries.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'countries-country-page',
@@ -7,6 +11,26 @@ import { Component } from '@angular/core';
   ]
 })
 
-export class CountryPageComponent {
+export class CountryPageComponent implements OnInit {
+
+  public country: Country | null = null
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countriesService: CountriesService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.countriesService.searchCountryByAlphaCode(id))
+      )
+      .subscribe((country) => {
+        if (!country) return this.router.navigateByUrl('')
+        return this.country = country
+      })
+    // throw new Error('Method not implemented.');
+  }
 
 }
