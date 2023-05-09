e7ba9fe46892c46f406ab8ef8d7142eedf1a21e0
Daniela Liendo <daniela@ualabee.com>
2023-05-09 22:39:48 +0200

    initial value was added to search box input

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -19,6 +19,9 @@ export class SearchBoxComponent implements OnInit, OnDestroy {
   @Input()
   public placeholder: string = ''
 
+  @Input()
+  public initialValue: string = ''
+
   @Output()
   public onValue = new EventEmitter<string>()
 
@@ -31,6 +34,7 @@ export class SearchBoxComponent implements OnInit, OnDestroy {
       .pipe(
         debounceTime(300)
       )
+
       .subscribe(value => {
         this.onDebounce.emit(value)
       })
